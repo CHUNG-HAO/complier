6f72398be487bc8788e75a7705075341f8017df9
chris <chunghao777@gmail.com>
2023-12-30 22:36:14 +0800

    add test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+using namespace std;
+
 using boost::asio::ip::udp;
 
 int main() {
@@ -23,12 +25,17 @@ int main() {
 
     while (true) {
         socket.send_to(boost::asio::buffer(message), remote_endpoint);
+        cout << "Sent " << message.size() << " bytes to " << remote_endpoint << endl;
+
         if (target_port == 65534) {
             target_port = 1;
         } else {
             ++target_port;
         }
-    }
+
+        // Sleep for 1 millisecond
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    } // Add this closing brace
 
     return 0;
 }
